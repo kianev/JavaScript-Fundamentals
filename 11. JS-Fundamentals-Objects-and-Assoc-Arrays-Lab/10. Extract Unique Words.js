c89aaf33d14412c5b1dba345785c501fb4fedcb1
Kaloian Ianev <kianev@gmail.com>
2017-04-04 21:47:34 +0300

    last one

---
+++ 11. JS-Fundamentals-Objects-and-Assoc-Arrays-Lab/10. Extract Unique Words.js
@@ -0,0 +1,19 @@
+function extractWords(sentences) {
+    let wordPattern = /\b[a-zA-Z]+\b/g;
+    let words = new Set();
+    for (let sentence of sentences) {
+        let matches = sentence.match(wordPattern);
+        matches.forEach(x => words.add(x.toLowerCase()));
+    }
+    console.log([...words.values()].join(", "));
+    //console.log(Array.from(words.values()).join(", "));
+}
+extractWords([
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis hendrerit dui.',
+        'Quisque fringilla est urna, vitae efficitur urna vestibulum fringilla.',
+        'Vestibulum dolor diam, dignissim quis varius non, fermentum non felis.',
+        'Vestibulum ultrices ex massa, sit amet faucibus nunc aliquam ut.',
+        'Morbi in ipsum varius, pharetra diam vel, mattis arcu.',
+        'Integer ac turpis commodo, varius nulla sed, elementum lectus.',
+        'Vivamus turpis dui, malesuada ac turpis dapibus, congue egestas metus.'
+]);
